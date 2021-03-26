b319ab65e5e0d2f6fb04a5fda2652f2a431995b6
syntiam <78595629+syntiam@users.noreply.github.com>
2021-03-26 10:54:09 +0100

    Create capitalizeFirstLetters.js

---
+++ capitalizeFirstLetters.js
@@ -0,0 +1,5 @@
+const assert = require ("assert");
+assert.strictEqual(typeOf capitalizeFirstLetters, ("function");
+assert strictEqual(capitalizeFirstLetters.length, 3);
+assert strictEqual(capitalizeFirstLetters("a"), A);
+assert strictEqual(capitalizeFirstLetters(""), "");
